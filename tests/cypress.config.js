c81f343a920d33303ce5ff13c2daacf5f4aeafc6
cadumega <72901445+cadumega@users.noreply.github.com>
2025-02-28 02:31:21 -0300

    Update cypress.config.js

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      specPattern: 'cypress/e2e/**/*.cy.js', // Where your tests are
+      supportFile: 'cypress/support/e2e.js', // Where your support file is
     },
   },
 })
