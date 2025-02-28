94a99fb7e6773f596a0eb8d21bf0f7f8e71de0ad
cadumega <72901445+cadumega@users.noreply.github.com>
2025-02-28 17:00:11 -0300

    Update cypress.config.js

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+    specPattern: '**/*.cy.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
