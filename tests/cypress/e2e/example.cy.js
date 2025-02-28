9a6f794fb03dcd2473347ff001845c97240f16e4
cadumega <72901445+cadumega@users.noreply.github.com>
2025-02-28 17:03:53 -0300

    Delete tests/cypress/e2e/example.cy.js

--- tests/cypress/e2e/example.cy.js
+++
@@ -1,8 +0,0 @@
-// tests/example.cy.js
-describe('Basic test', () => {
-  it('loads the homepage', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('h1', 'Welcome').should('be.visible')
-    // If you don't have a "Welcome" h1, replace with something you actually have on your page
-  })
-})
